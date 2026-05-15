c0b074247d915e95dc69d1e11e99d4b903ce306b
AliisaIhmemaassa <aliisa.h.maki@gmail.com>
2026-05-15 18:37:47 +0300

    Add files via upload

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,3 +1,4 @@
+
 const toggleButton = document.createElement('button');
 toggleButton.onclick = toggleTheme;
 document.querySelector('.navbar').appendChild(toggleButton);
@@ -5,33 +6,22 @@ document.querySelector('.navbar').appendChild(toggleButton);
 applyTheme();
 
 function applyTheme() {
-    const sheet = document.querySelector('link[rel="stylesheet"]');
-    const page = sheet.dataset.page;
-
     if (localStorage.getItem('theme') === 'dark') {
-        if (page === 'style1') {sheet.href = `styles/${page}-dark.css`;} 
-        else {sheet.href = `../styles/${page}-dark.css`;}
+        document.documentElement.setAttribute('data-theme', 'dark');
         toggleButton.textContent = 'Vaalea teema';
-    }
-    else {
-        if (page === 'style1') {sheet.href = `styles/${page}.css`;} 
-        else {sheet.href = `../styles/${page}.css`;}
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
         toggleButton.textContent = 'Tumma teema';
     }
 }
 
 function toggleTheme() {
-    const sheet = document.querySelector('link[rel="stylesheet"]');
-    const page = sheet.dataset.page;
-
     if (localStorage.getItem('theme') === 'dark') {
-        if (page === 'style1') {sheet.href = `styles/${page}.css`;} 
-        else {sheet.href = `../styles/${page}.css`;}
+        document.documentElement.setAttribute('data-theme', 'light');
         toggleButton.textContent = 'Tumma teema';
         localStorage.setItem('theme', 'light');
     } else {
-        if (page === 'style1') {sheet.href = `styles/${page}-dark.css`;} 
-        else {sheet.href = `../styles/${page}-dark.css`;}
+        document.documentElement.setAttribute('data-theme', 'dark');
         toggleButton.textContent = 'Vaalea teema';
         localStorage.setItem('theme', 'dark');
     }
